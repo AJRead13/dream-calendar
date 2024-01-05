08c20370bf4a2143ed372935f096af2f5aeaaa16
Pandax19 <ajwhitley@comcast.net>
2024-01-05 00:52:34 -0500

    added some dream questions in content section

---
+++ client/src/components/Content/index.jsx
@@ -0,0 +1,14 @@
+// how many senses were active? smell, touch, taste, etc
+//significant colors, shapes or symbols around you
+//what was the setting
+//what emotions were you feeling during the dream
+//what was the overall mood 
+//significant or recurring symbols/themes
+//who else was there and what role did they play (friend, foe)
+//what thoughts did your dream self have if any
+//how did you initially interpret the dream 
+//do you want to name the dream
+//what were your last conscious thoughts before falling asleep 
+//
+//**a lot of sites say to just start word-vomiting your dream first and then analyze so maybe consider putting the big text field first, then have the prompts to dissect after **/ 
+//* also optional - being able to put how long they slept for. that might have an impact on dreams but i havent read anything about that yet*
